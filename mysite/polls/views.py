c2aac03aedc9271c3143471bbe207a93d4057690
bhanna <bhanna1693@gmail.com>
2019-08-16 22:24:06 -0400

    created corresponding urls for views (edited view names)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,10 +5,14 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def result(request, question_id):
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
     response = "You're looking at the results of uestion %s."
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You'r voting on question %s." % question_id)
+    return HttpResponse("You're voting on question %s." % question_id)
