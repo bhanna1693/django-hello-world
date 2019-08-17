33db82c1f9615d5868289d61518049249d2a3487
bhanna <bhanna1693@gmail.com>
2019-08-17 00:25:56 -0400

    template 'url' syntax added and 'app_name' variable added to polls.url for multi app route identification

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 # /polls/
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
